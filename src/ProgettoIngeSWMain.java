026eee95a22227b0890b7a8262b47e880f9c52d5
UNIBS-Reds <p.rossi017@studenti.unibs.it>
2014-10-17 17:01:10 +0200

    Aggiunta menu principale.

--- src/ProgettoIngeSWMain.java
+++ src/ProgettoIngeSWMain.java
@@ -1,11 +1,14 @@
 
 public class ProgettoIngeSWMain {
 
+	public final static String TITOLO_PRINCIPALE = "Menu Principale";
+	public final static String[] OPZIONI_PRINCIPALE = {"Inserimento nuovo modello", "Carica modello"};
+	public final static String TITOLO_INSERIMENTO_MODELLO = "Menu Inserimento"; 
+	
 	public static void main(String[] args) {
-		
-	/*
+	
 		Modello mod = new Modello();
-		mod.creaNodoIniziale();
+		/*mod.creaNodoIniziale();
 		mod.creaAzione(null);
 		mod.creaAzione(null);
 		int b = mod.creaBranch();
@@ -21,5 +24,24 @@ public class ProgettoIngeSWMain {
 		mod.creaNodoFinale();
 		mod.stampaModello();
 	*/
+		
+		boolean finito = false;
+		
+		Menu menuPrincipale = new Menu(TITOLO_PRINCIPALE, OPZIONI_PRINCIPALE);
+		
+		do {
+		
+			switch(menuPrincipale.scegli()) {
+			
+				case 0: finito = true; break;
+				case 1: menuInserimento();
+				case 2: caricamentoModello();
+			}
+		} while(!finito);
 	}
+		
+	public static void menuInserimento() {};
+	
+	public static void caricamentoModello() {};
+		
 }
