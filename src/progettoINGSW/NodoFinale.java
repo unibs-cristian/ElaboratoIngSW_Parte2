df0968877321d2729df4db7e5abb4eaba9e023d9
unibs-cristian <c.sampietri@studenti.unibs.it>
2014-10-21 13:29:37 +0200

    Update NodoFinale.java

--- src/progettoINGSW/NodoFinale.java
+++ src/progettoINGSW/NodoFinale.java
@@ -2,11 +2,20 @@ package progettoINGSW;
 
 public class NodoFinale extends Entita 
 {
-	
-	public final static String ID_TIPO = "NODO_FINALE";
+	public final static String id_tipo = "NODO_FINALE";
 	
 	public NodoFinale(int id) {
 	
-		super(ID_TIPO);
+		super(id_tipo);
+	}
+	
+	public String toString()
+	{
+		StringBuffer risultato = new StringBuffer();
+		risultato.append(CORNICE);
+		risultato.append(String.format(MSG_ENTITA, super.getId(), id_tipo));
+		risultato.append("\n");
+		risultato.append(super.toString());
+		return risultato.toString();
 	}
 }
