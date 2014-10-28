383220769063f77563c3335e2b1ee3e707f4ada9
unibs-cristian <c.sampietri@studenti.unibs.it>
2014-10-28 13:10:43 +0100

    Introduzione classe coppia

--- src/testSuiteDiagnosi/ClasseEquivalenza.java
+++ src/testSuiteDiagnosi/ClasseEquivalenza.java
@@ -1,5 +1,22 @@
 package testSuiteDiagnosi;
+import java.util.Vector;
 
 public class ClasseEquivalenza {
 
+	private int cardinalita;
+	/** Attributo che contiene le coppie (Insieme del cammino, valore della rilevazione */
+	private Vector<Coppia> elencoCoppie;
+
+	public ClasseEquivalenza(int _cardinalita) {
+		cardinalita = _cardinalita;
+		elencoCoppie = new Vector<Coppia>(); 
+	}
+	
+	public int getCardinalita() {
+		return cardinalita;
+	}
+	
+	public Vector <Coppia> getElencoCoppie() {
+		return elencoCoppie;
+	}
 }
