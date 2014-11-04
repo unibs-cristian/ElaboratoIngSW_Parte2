21b487958bd7576a5aa1b8b3967d5abf6082c21e
Studente <Studente@PC10.info2.ing.unibs.it>
2014-11-04 13:57:25 +0100

    fix

--- src/testSuiteDiagnosi/ProbabilitaMetodo1.java
+++ src/testSuiteDiagnosi/ProbabilitaMetodo1.java
@@ -8,13 +8,15 @@ public class ProbabilitaMetodo1 implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	public Vector<Float> calcolaProbabilita (TestSuite testSuite, Vector<Vector<Integer>> vettoreRisultatiTestSuite)
-	{
+	{		
 		Vector<Float> risultato = new Vector<Float>(); 
 		Vector<Vector<Integer>> vettoreRisultatiProbabilitaClassi = new Vector<Vector<Integer>>();
 		for ( int i = 0; i < testSuite.getElencoClassi().size(); i++)
 		{
 			Vector<Integer> risultatoProbabilitaClasse = new Vector<Integer>();
 			int cardinalita = testSuite.getElencoClassi().get(i).getCardinalita();
+			
+			System.out.println("cardinalità: " + cardinalita);
 			
 			if (cardinalita > 1)
 				risultatoProbabilitaClasse = calcolaProbabilitaClasse(cardinalita, vettoreRisultatiTestSuite.get(i) );
@@ -30,12 +32,16 @@ public class ProbabilitaMetodo1 implements Serializable
 		for (int i = 0; i < vettoreRisultatiProbabilitaClassi.size(); i++)
 		{
 			int cardinalita = testSuite.getElencoClassi().get(i).getCardinalita();
-			
+			System.out.println("cardinalità: " + cardinalita);
+			numeratoreClassi.add(i, (float) 0);
+			denominatoreClassi.add(i, (float) 0);
 			for (int j = 0; j < vettoreRisultatiProbabilitaClassi.get(i).size(); j++)
 			{
 				if (vettoreRisultatiProbabilitaClassi.get(j).get(i) != -1)
 					{
-						numeratoreClassi.set(i, numeratoreClassi.get(i) + vettoreRisultatiProbabilitaClassi.get(i).get(j) );
+						System.out.println(vettoreRisultatiProbabilitaClassi.get(j).get(i));
+						numeratoreClassi.set(i, numeratoreClassi.get(i) + vettoreRisultatiProbabilitaClassi.get(j).get(i) );
+						System.out.println("numeratore " + i + " : " + numeratoreClassi.get(i) );
 						denominatoreClassi.set(i, denominatoreClassi.get(i) + cardinalita );
 					}
 			}
