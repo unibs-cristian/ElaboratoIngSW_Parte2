763cf95f5b9e57ef73cc2ec9c3558a9ec94d3127
unibs-cristian <c.sampietri@studenti.unibs.it>
2015-01-05 20:15:11 +0100

    Update RamoNonPercorso.java

--- src/controlloCammino/RamoNonPercorso.java
+++ src/controlloCammino/RamoNonPercorso.java
@@ -19,26 +19,26 @@ public class RamoNonPercorso implements StatoCammino{
 	/** Costante per il salvataggio */
 	private static final long serialVersionUID = 1L;
 
-	public void gestisciStato(CamminoAzioni camm, String stato) {
-		if(stato.equals(STATO_NON_OK))
-			camm.setStatoCammino(new StatoNonOk());
-		else if(stato.equals(ENTRATO_RAMO))
-			camm.setStatoCammino(new EntratoRamo());
-		else if(stato.equals(STATO_OK))
-			camm.setStatoCammino(new StatoOk());
-		else if(stato.equals(SALTATO_BLOCCO))
-			camm.setStatoCammino(new SaltatoBlocco());
-		else if(stato.equals(PERCORSO_TUTTO))
-			camm.setStatoCammino(new PercorsoTutto());
-		else if(stato.equals(FERMATO))
-			camm.setStatoCammino(new Fermato());
-		else if(stato.equals(FERMATO_DENTRO))
-			camm.setStatoCammino(new FermatoDentro());
+	public void gestisciStatoCammino(CamminoAzioni camminoDaGestire, String nuovoStatoCammino) {
+		if(nuovoStatoCammino.equals(STATO_NON_OK))
+			camminoDaGestire.setStatoCammino(new StatoNonOk());
+		else if(nuovoStatoCammino.equals(ENTRATO_RAMO))
+			camminoDaGestire.setStatoCammino(new EntratoRamo());
+		else if(nuovoStatoCammino.equals(STATO_OK))
+			camminoDaGestire.setStatoCammino(new StatoOk());
+		else if(nuovoStatoCammino.equals(SALTATO_BLOCCO))
+			camminoDaGestire.setStatoCammino(new SaltatoBlocco());
+		else if(nuovoStatoCammino.equals(PERCORSO_TUTTO))
+			camminoDaGestire.setStatoCammino(new PercorsoTutto());
+		else if(nuovoStatoCammino.equals(FERMATO))
+			camminoDaGestire.setStatoCammino(new Fermato());
+		else if(nuovoStatoCammino.equals(FERMATO_DENTRO))
+			camminoDaGestire.setStatoCammino(new FermatoDentro());
 	}
 	
-	public void gestisciStatoRamo(Ramo r, String stato) {
-		if(stato.equals(STATO_NON_OK))
-			r.setStato(new StatoNonOk());
+	public void gestisciStatoRamo(Ramo ramoDaGestire, String nuovoStatoRamo) {
+		if(nuovoStatoRamo.equals(STATO_NON_OK))
+			ramoDaGestire.setStato(new StatoNonOk());
 	}
 	
 	public boolean isValid() {
