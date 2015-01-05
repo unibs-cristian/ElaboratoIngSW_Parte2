68dc7be44a8133d34d4e0628b7b24328154ad431
unibs-cristian <c.sampietri@studenti.unibs.it>
2015-01-05 20:15:51 +0100

    Update RamoPercorsoParz.java

--- src/controlloCammino/RamoPercorsoParz.java
+++ src/controlloCammino/RamoPercorsoParz.java
@@ -18,22 +18,22 @@ public class RamoPercorsoParz implements StatoCammino{
 	/** Costante per il salvataggio */
 	private static final long serialVersionUID = 1L;
 
-	public void gestisciStato(CamminoAzioni camm, String stato) {
-		if(stato.equals(FERMATO_DENTRO))
-			camm.setStatoCammino(new FermatoDentro());
-		else if(stato.equals(PERCORSO_TUTTO))
-			camm.setStatoCammino(new PercorsoTutto());   //ci vado se l'azione inserita e' l'ultima del ramo
-		else if(stato.equals(ENTRATO_RAMO))
-			camm.setStatoCammino(new EntratoRamo());
+	public void gestisciStatoCammino(CamminoAzioni camminoDaGestire, String nuovoStatoCammino) {
+		if(nuovoStatoCammino.equals(FERMATO_DENTRO))
+			camminoDaGestire.setStatoCammino(new FermatoDentro());
+		else if(nuovoStatoCammino.equals(PERCORSO_TUTTO))
+			camminoDaGestire.setStatoCammino(new PercorsoTutto());   //ci vado se l'azione inserita e' l'ultima del ramo
+		else if(nuovoStatoCammino.equals(ENTRATO_RAMO))
+			camminoDaGestire.setStatoCammino(new EntratoRamo());
 	}
 	
-	public void gestisciStatoRamo(Ramo r, String stato) {
-		if(stato.equals(FERMATO_DENTRO))
-			r.setStato(new FermatoDentro());
-		else if(stato.equals(PERCORSO_TUTTO))
-			r.setStato(new PercorsoTutto());
-		else if(stato.equals(STATO_NON_OK))
-			r.setStato(new StatoNonOk());
+	public void gestisciStatoRamo(Ramo ramoDaGestire, String nuovoStatoRamo) {
+		if(nuovoStatoRamo.equals(FERMATO_DENTRO))
+			ramoDaGestire.setStato(new FermatoDentro());
+		else if(nuovoStatoRamo.equals(PERCORSO_TUTTO))
+			ramoDaGestire.setStato(new PercorsoTutto());
+		else if(nuovoStatoRamo.equals(STATO_NON_OK))
+			ramoDaGestire.setStato(new StatoNonOk());
 	}
 
 	public boolean isValid() {
