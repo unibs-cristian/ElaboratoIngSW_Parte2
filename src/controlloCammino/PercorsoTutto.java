f78ca9171dc756e803a7a1ec4ae98ec1ad44b279
unibs-cristian <c.sampietri@studenti.unibs.it>
2015-01-05 20:14:13 +0100

    Update PercorsoTutto.java

--- src/controlloCammino/PercorsoTutto.java
+++ src/controlloCammino/PercorsoTutto.java
@@ -15,18 +15,18 @@ public class PercorsoTutto implements StatoCammino {
 	/** Costante per il salvataggio */
 	private static final long serialVersionUID = 1L;
 
-	public void gestisciStato(CamminoAzioni camm, String stato) {
-		if(stato.equals(STATO_NON_OK))
-			camm.setStatoCammino(new StatoNonOk());
-		else if(stato.equals(ENTRATO_RAMO))
-			camm.setStatoCammino(new EntratoRamo());
-		else if(stato.equals(STATO_OK))
-			camm.setStatoCammino(new StatoOk());
-		else if(stato.equals(PERCORSO_PARZ))
-			camm.setStatoCammino(new RamoPercorsoParz());
+	public void gestisciStatoCammino(CamminoAzioni camminoDaGestire, String nuovoStatoCammino) {
+		if(nuovoStatoCammino.equals(STATO_NON_OK))
+			camminoDaGestire.setStatoCammino(new StatoNonOk());
+		else if(nuovoStatoCammino.equals(ENTRATO_RAMO))
+			camminoDaGestire.setStatoCammino(new EntratoRamo());
+		else if(nuovoStatoCammino.equals(STATO_OK))
+			camminoDaGestire.setStatoCammino(new StatoOk());
+		else if(nuovoStatoCammino.equals(PERCORSO_PARZ))
+			camminoDaGestire.setStatoCammino(new RamoPercorsoParz());
 	}
 	
-	public void gestisciStatoRamo(Ramo r, String stato) {}
+	public void gestisciStatoRamo(Ramo ramoDaGestire, String nuovoStatoRamo) {}
 	
 	public boolean isValid() {
 		return false;
