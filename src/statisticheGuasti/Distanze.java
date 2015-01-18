20a9e8e2cf94eeb8b804aa77b3c4b47149292488
LorenzoRubagotti <l.rubagotti@studenti.unibs.it>
2015-01-18 14:54:08 +0100

    Aggiunta di Serializable in Distanze e ora il report è salvato con successo

--- src/statisticheGuasti/Distanze.java
+++ src/statisticheGuasti/Distanze.java
@@ -3,6 +3,7 @@
  */
 package statisticheGuasti;
 
+import java.io.Serializable;
 import java.util.Vector;
 
 import gestioneModello.Azione;
@@ -12,7 +13,10 @@ import gestioneModello.Modello;
 /**
  * The Class Distanze.
  */
-public class Distanze {
+public class Distanze implements Serializable {
+
+	/** Costante per il salvataggio */
+	private static final long serialVersionUID = 1L;
 
 	/** The vettore metodo1. */
 	private Vector<int[]> vettoreMetodo1;
